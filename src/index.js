a38498c7b9935defa600c58f57bc53118f1b67ce
linfan <linfan@yunfan.com>
2018-04-20 22:31:14 +0800

    commit demo

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var func = function(str) {
+	document.getElementById('app').innerHTML = str;
+};
+
+func("I am using Babel now!");
+// document.getElementById('app').innerHTML = "I am using Babel now!";
